18e288f1821fd3f780bb49672807562e4f3f1344
muskanvk <muskanvk@gmail.com>
2020-08-10 11:49:54 +0530

    Add Solution

---
+++ Shuffle_Array.cpp
@@ -0,0 +1,22 @@
+//Array Shuffling
+// https://leetcode.com/problems/shuffle-the-array/submissions/
+
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans;
+        int j = n;
+        for(int i = 0; i<n; i++)
+        {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+             j++;
+        }
+       
+        
+        return ans;
+            
+        
+    }
+};
