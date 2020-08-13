093c625fdca46de69a512846cf96e1cb43830465
muskanvk <muskanvk@gmail.com>
2020-08-13 23:41:00 +0530

    Please enter the commit message for your changes. Lines starting

---
+++ Easy Problems/Fibonacci_Number.cpp
@@ -0,0 +1,13 @@
+// 509. Fibonacci Number
+
+class Solution {
+public:
+    int fib(int N) {
+        if (N <= 1)
+        {
+            return N;
+        } 
+        return  fib(N-1) + fib(N-2);
+        
+    }
+};
